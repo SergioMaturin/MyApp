979a6202e4c25530299f32988f5c329a9bd7e4b5
DESKTOP-L0SRT13\Skif <kutsynda@ukr.net>
2018-05-21 23:15:09 +0300

    My fist commit.

---
+++ MyApp/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tt = 4;
+            var yy = 5;
+
+            var ee = tt + yy;
+            Console.WriteLine(ee);
+            Console.Read();
+
+        }
+    }
+}
